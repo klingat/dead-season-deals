4aa067710b362854d5aeeeea3fd82d943652ef1f
Kash <klingat@me.com>
2019-10-05 15:04:27 -0700

    Add and style action btns (website, phone)

--- src/components/DealToggle.js
+++ src/components/DealToggle.js
@@ -17,21 +17,29 @@ class DealToggle extends Component {
     const deal = this.props.deal;
     return (
       <div className="DealToggle box-shadow" onClick={this.toggleDealDetails}>
-        <div className="dealTitle">
+        <div className="DealToggle--title">
           <h3>{deal.dealTitle}</h3>
           <p>{deal.restaurantName}</p>
         </div>
 
         {this.state.isOpened && (
-          <div className="dealContent">
+          <div className="DealToggle--content">
             <ul>
               {deal.dealDetails.map((detail, i) => (
                 <li key={i}>{detail}</li>
               ))}
             </ul>
-            <p>{deal.website}</p>
-            <p>{deal.phoneNumber}</p>
-            <img src={deal.image} className="source-image" />
+            <div className="DealToggle--actions">
+              <a id="call-action" href={`tel:${deal.phoneNumber}`}>
+                <i className="fas fa-phone-alt"></i> Call
+              </a>
+              <a id="website-action" href={deal.website} target="_blank">
+                <i className="fas fa-globe"></i> Website
+              </a>
+            </div>
+            <a id="source-action" href={deal.image} target="_blank">
+              Source
+            </a>
           </div>
         )}
       </div>
